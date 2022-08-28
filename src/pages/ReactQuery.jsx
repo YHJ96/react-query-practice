b0211b305c3f9ff4c89de6d2eda5ff30ae379ab0
YHJ96 <yhj878812@naver.com>
2022-08-28 16:54:56 +0900

    feat: useQuery 확인 완료

--- src/pages/ReactQuery.jsx
+++ src/pages/ReactQuery.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+const fetchAnimal = () => axios.get('/animal');
 
 function ReactQuery() {
-  return <div>ReactQuery</div>;
+  const { data, isLoading } = useQuery(['animal'], fetchAnimal);
+
+  if (isLoading) return <div>Loding</div>;
+
+  const createData = () => {
+    return data?.data.map((item) => {
+      return <div key={item.id}>{item.name}</div>;
+    });
+  };
+
+  return <React.Fragment>{createData()}</React.Fragment>;
 }
 
 export default ReactQuery;
