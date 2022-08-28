49c2f76d357d1572c863ffd32dcf4c21388ca2b6
YHJ96 <yhj878812@naver.com>
2022-08-28 16:55:23 +0900

    feat: react-state 방법 제공 완료

--- src/pages/ReactState.jsx
+++ src/pages/ReactState.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function ReactState() {
-  return <div>ReactState</div>;
+  const [isLoding, setIsLoding] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('/animal').then((res) => {
+      setData(res.data);
+      setIsLoding(false);
+    });
+  }, []);
+
+  if (isLoding) return <div>Loding</div>;
+
+  const createData = () => {
+    return data.map((item) => {
+      return <div key={item.id}>{item.name}</div>;
+    });
+  };
+
+  return <React.Fragment>{createData()}</React.Fragment>;
 }
 
 export default ReactState;
