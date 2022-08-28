e2e18031ef2bef11354c9322a569df4101790526
YHJ96 <yhj878812@naver.com>
2022-08-28 16:55:54 +0900

    feat: react-query 스토어 제공 완료

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 const root = document.getElementById('root');
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>,
 );
