63ad38848b3c2ae6fc8a498054d8aa1d688542e8
YHJ96 <yhj878812@naver.com>
2022-09-03 23:50:29 +0900

    feat: cacheTime & staleTime 옵션 확인 완료

--- src/pages/ReactQuery.jsx
+++ src/pages/ReactQuery.jsx
@@ -5,7 +5,13 @@ import { useQuery } from '@tanstack/react-query';
 const fetchAnimal = () => axios.get('/animal');
 
 function ReactQuery() {
-  const { data, isLoading, isError, error } = useQuery(['animal'], fetchAnimal);
+  /* cacheTime뒤에 가비지 컬렉터을 사용해서 제거 */
+  /* staleTime뒤에 fetch 네트워크에 fetch 요청 전송 */
+  const { data, isLoading, isError, error } = useQuery(
+    ['animal'],
+    fetchAnimal,
+    { cacheTime: 5000, staleTime: 30000 },
+  );
 
   if (isLoading) return <div>Loding</div>;
 
