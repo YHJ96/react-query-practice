dfec3aa5cef6c32797ec87ed98df2eaeb7be7206
YHJ96 <yhj878812@naver.com>
2022-09-24 19:52:44 +0900

    feat: onSuccess & onError 옵션 확인 완료

--- src/pages/ReactQuery.jsx
+++ src/pages/ReactQuery.jsx
@@ -8,10 +8,16 @@ function ReactQuery() {
   /* cacheTime뒤에 가비지 컬렉터을 사용해서 제거 */
   /* staleTime뒤에 fetch 네트워크에 fetch 요청 전송 */
   /* refetch로 클릭이벤트 활용 가능  */
+  /* onSuccess, onError 콜백함수 활용 가능  */
+
+  const onSuccess = () => console.log('OnSucess');
+
+  const onError = () => console.log('OnError');
+
   const { data, isLoading, isError, error, isFetching, refetch } = useQuery(
     ['animal'],
     fetchAnimal,
-    { enabled: false },
+    { enabled: false, onSuccess, onError },
   );
 
   if (isLoading && isFetching) return <div>Loding</div>;
