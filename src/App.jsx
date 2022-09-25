e45e30d0d654c91a68d0cd58164f89a2a783e07f
YHJ96 <yhj878812@naver.com>
2022-09-25 21:02:34 +0900

    feat: Query 병렬 확인 완료

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactQuery from './pages/ReactQuery';
 import ReactState from './pages/ReactState';
 import ReactQueryDetail from './pages/ReactQueryDetail';
+import Test1 from './pages/Test1';
 import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
@@ -13,8 +14,12 @@ function App() {
       <Link to="/query">
         <li>React-Query</li>
       </Link>
+      <Link to="/test1">
+        <li>Test1</li>
+      </Link>
       <Routes>
         <Route path="/" element={<ReactState />} />
+        <Route path="/test1" element={<Test1 />} />
         <Route path="/query" element={<ReactQuery />} />
         <Route path="/query/:id" element={<ReactQueryDetail />} />
       </Routes>
